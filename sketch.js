e58f168b0624a71ea75b5f4761e385a08d9e87fd
thwishaa <64151507+thwishaa@users.noreply.github.com>
2020-05-23 14:13:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 var engine, world;
 
-var rect1,rect2,rect3,rect4;
+var rect1,rect2,rect3,rect4,rect5;
 
 
 
 function setup() {
   engine = Engine.create();
-  createCanvas(800,400);
+  createCanvas(800,800);
 
 world = engine.world;
 var options = {
@@ -18,14 +18,17 @@ var options = {
 rect1 = Bodies.rectangle(150,190,25,350,options);
 World.add(world,rect1);
 
+
 rect2 = Bodies.rectangle(750,190,25,350,options);
 World.add(world,rect2);
 
-rect3 = Bodies.rectangle(450,50,600,25,options);
+rect3 = Bodies.rectangle(450,150,25,350,options);
 World.add(world,rect3);
 
-rect4 = Bodies.rectangle(450,650,600,25,options);
+rect4 = Bodies.rectangle(500,190,25,350,options);
 World.add(world,rect4);
+
+rect5 = Bodies.rectangle(400,380,800,20,options)
   //createSprite(400, 200, 50, 50);
 }
 
@@ -33,9 +36,18 @@ function draw() {
   background(0);  
   Engine.update(engine);
   rectMode(CENTER);
+  fill("pink");
   rect(rect1.position.x,rect1.position.y,25,350);
+  fill("pink");
   rect(rect2.position.x,rect2.position.y,25,350);
-  rect(rect3.position.x,rect2.position.y,600,25);
-  rect(rect4.position.x,rect2.position.y,600,25);
+  fill("pink");
+  rect(rect3.position.x,rect2.position.y,25,350);
+  fill("pink");
+  rect(rect4.position.x,rect2.position.y,25,350);
+  fill("pink");
+  rect(rect5.position.x,rect5.position.y,800,20);
+  
+
+  
   drawSprites();
 }
